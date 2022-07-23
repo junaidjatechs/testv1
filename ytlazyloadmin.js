9b6abf2eb7376b2a0c459f4d18d56a8a82c5afd7
junaidjatechs <108971187+junaidjatechs@users.noreply.github.com>
2022-07-24 02:28:53 +0500

    Update ytlazyloadmin.js

--- ytlazyloadmin.js
+++ ytlazyloadmin.js
@@ -1,1 +1,31 @@
-!function(){let b=document.querySelectorAll(".ja-embed-youtube");for(let a=0;a<b.length;a++){let c=b[a],e="https://img.youtube.com/vi/"+c.dataset.videoId+"/sddefault.jpg",d=new Image;d.src=e,d.addEventListener("load",function(){c.appendChild(d)}),c.addEventListener("click",function(){let a=document.createElement("iframe");a.setAttribute("frameborder","0"),a.setAttribute("allowfullscreen",""),a.setAttribute("allow","accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"),a.setAttribute("src","https://www.youtube.com/embed/"+this.dataset.videoId+"?rel=0&showinfo=0&autoplay=1"),this.innerHTML="",this.appendChild(a)})}}()
+(function(){
+  let YouTubeContainers = document.querySelectorAll(".ja-embed-youtube");
+
+  // Iterate over every YouTube container you may have
+  for (let i = 0; i < YouTubeContainers.length; i++) {
+    let container = YouTubeContainers[i];
+    let imageSource = "https://img.youtube.com/vi/"+ container.dataset.videoId +"/sddefault.jpg"; 
+
+    // Load the Thumbnail Image asynchronously
+    let image = new Image();
+    image.src = imageSource;
+    image.addEventListener("load", function() {
+      container.appendChild(image);
+    });
+
+    // When the user clicks on the container, load the embedded YouTube video
+    container.addEventListener("click", function() {
+      let iframe = document.createElement( "iframe" );
+
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("allowfullscreen", "");
+      iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
+      // Important: add the autoplay GET parameter, otherwise the user would need to click over the YouTube video again to play it 
+      iframe.setAttribute("src", "https://www.youtube.com/embed/"+ this.dataset.videoId +"?rel=0&showinfo=0&autoplay=1");
+
+      // Clear Thumbnail and load the YouTube iframe
+      this.innerHTML = "";
+      this.appendChild( iframe );
+    });
+  }
+})();
